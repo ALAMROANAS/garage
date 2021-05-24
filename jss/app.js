a4b36bc9977139a7622e61b1fe0ad68113bd6d23
Mrsycho <>
2021-05-24 01:29:29 +0000

    asking

---
+++ jss/app.js
@@ -0,0 +1,14 @@
+alert('welcome')
+
+let VehicleType = prompt('what is Vehicle Type?');
+console.log(VehicleType);
+
+let Vehiclemodel = prompt('what is the Vehicle model?');
+console.log(vehiclemodel);
+
+let whatfix = prompt('you need fix your car?');
+console.log(whatfix);
+
+let whenfix = prompt('when you want to fix it?');
+console.log(whenfix);
+
