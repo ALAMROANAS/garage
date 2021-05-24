6cbc90b1519e20423b9e58b4e6e9513ba75311fc
Anas Alamro <alamroanas@gmail.com>
2021-05-24 21:57:00 +0300

    fix

--- js/app.js
+++ js/app.js
@@ -3,8 +3,10 @@ alert('welcome')
 let VehicleType = prompt('what is Vehicle Type?');
 console.log(VehicleType);
 
-let Vehiclemodel = prompt('what is the Vehicle model?');
-console.log(vehiclemodel);
+
+let carmodel = prompt('what is the Vehicle model?');
+console.log(carmodel);
+
 
 let whatfix = prompt('you need fix your car?');
 console.log(whatfix);
